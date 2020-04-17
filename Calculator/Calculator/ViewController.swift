10dfa1b89b2850dbe9c46229c74a34f71f518326
MagnaPax <chunhee.han@outlook.com>
2020-04-17 14:17:17 +0900

    [#3] feat : Connect objects to Code

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -10,11 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    private var calculatedResult: [String?] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+    //MARK: Actions
+    @IBAction func touchKeypad(_ sender: UIButton) {
+        
+        calculatedResult.append("hi")
+        print("keypad touched")
+        print(calculatedResult.index(after: 0))
+        display.text = sender.currentTitle
+    }
+    
 }
 
