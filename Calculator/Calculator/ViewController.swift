cd57d9e6e8eb7abc170fd5594aa4c5bada9b28e4
MagnaPax <chunhee.han@outlook.com>
2020-04-17 15:23:10 +0900

    [#3] feat : Identify which keypad button entered.
    어떤 버튼이 눌렸는지 구별하기

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -14,22 +14,42 @@ class ViewController: UIViewController {
     @IBOutlet weak var display: UILabel!
     @IBOutlet weak var tableView: UITableView!
     private var calculatedResult: [String?] = []
+    private var unwrappedTouchedKey: String = ""
+    private let textCurrentlyInDisplay: String = ""
+    private var userIsInTheMiddleOfTyping = false
+    
+    
+    //MARK: Actions
+    @IBAction func touchKeypad(_ sender: UIButton) {
+        
+        // 어떤 버튼 눌렸는지 찾기
+        if let touchedKey = sender.currentTitle {
+            print("키패드에서 \(touchedKey) 눌렸음")
+            unwrappedTouchedKey = touchedKey
+        }
+        else {
+            fatalError("키패드에 아무것도 눌린 값이 없음")
+        }
+        
+        
+
+        
+        
+    }
+    
+    
+    
+    
+    
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
     
-    //MARK: Actions
-    @IBAction func touchKeypad(_ sender: UIButton) {
-        
-        calculatedResult.append("hi")
-        print("keypad touched")
-        print(calculatedResult.index(after: 0))
-        display.text = sender.currentTitle
-    }
+    
     
 }
 
